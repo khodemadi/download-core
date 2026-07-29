b5e0b634260612d45b0fbc7ae38de081be130ef9
madi <mz2284326@gmail.com>
2026-07-29 17:34:15 +0330

    Update lib.rs

--- crs/lib.rs
+++ crs/lib.rs
@@ -1,1 +1,10 @@
- 
+pub mod downloader;
+pub mod error;
+pub mod retry;
+pub mod segment;
+pub mod types;
+pub mod verify;
+
+pub use downloader::{DownloadManager, DownloadOptions, DownloadReport};
+pub use error::{DownloadError, Result};
+pub use types::{DownloadProgress, DownloadStatus, StarLevel}; 
